01be45e184e3569b97a9bd243a575075014eecb2
Fred Lahode <lahode@infomaniak.ch>
2022-06-23 11:19:13 +0200

    Add code in service

--- app/Http/Services/FilmService.php
+++ app/Http/Services/FilmService.php
@@ -5,48 +5,55 @@ namespace App\Http\Services;
 class FilmService {
 
     public function storeFilm($id) {
+        try {
 
-        $validatorRules = [
-            'title' => 'required|string|max:256',
-            'year' => 'required|digits:4|integer|min:1900|max:'.(date('Y')+1),
-            'description' => 'nullable|string',
-            'category_id' => 'integer|digits_between:1,20|required'
-        ];
+            $validatorRules = [
+                'title' => 'required|string|max:256',
+                'year' => 'required|digits:4|integer|min:1900|max:'.(date('Y')+1),
+                'description' => 'nullable|string',
+                'category_id' => 'integer|digits_between:1,20|required'
+            ];
 
-        // Validate fields.
-        $validator = Validator::make($request->all(), $validatorRules);
+            // Validate fields.
+            $validator = Validator::make($request->all(), $validatorRules);
 
-        // If validation fails
-        // Return error messages and exit.
-        if ($validator->fails()) {
-            throw (new ValidateException(
-                $validator->errors()
-            ));
-        }
-
-
-
-        $category = Category::find($request->input('category_id'));
-
-        $film = null;
-        if ($id) {
-            $film = Film::find($id);
-            if (!$film) {
-                throw new ApiException(
-                    "Film not found.",
-                    404
-                );
+            // If validation fails
+            // Return error messages and exit.
+            if ($validator->fails()) {
+                throw (new ValidateException(
+                    $validator->errors()
+                ));
             }
-        } else {
-            $film = new Film();
+
+
+
+            $category = Category::find($request->input('category_id'));
+
+            $film = null;
+            // Update film.
+            if ($id) {
+                $film = Film::find($id);
+                if (!$film) {
+                    throw new ApiException(
+                        "Film not found.",
+                        404
+                    );
+                }
+            // Create film.
+            } else {
+                $film = new Film();
+            }
+
+            $film->title = $request->input('title');
+            $film->year = $request->input('year');
+            $film->description = $request->input('description');
+            $film->category_id = $category->id;
+
+            $film->save();
         }
-
-        $film->title = $request->input('title');
-        $film->year = $request->input('year');
-        $film->description = $request->input('description');
-        $film->category_id = $category->id;
-
-        $film->save();
+        catch(\Exception $e) {
+            throw $e;
+        }
     }
 
 }
